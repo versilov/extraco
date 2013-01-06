5d2e07c28a4584d08cad1d3169351a5155b75f01
Stas Versilov <stas.versilov@gmail.com>
2013-01-06 23:34:05 +0400

    The Start.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Extraco::Application.config.secret_token = '0016239d62a81f43605bed71f935b6e9ca5610c2b44a2ba39bf75666583556e294e1cfa09df6372609b6539f5587396b99a58845270291961bf5bbe11b90b15f'
