89f108ed46f5eb7067afca64b1c855aa70f93a20
Stas Versilov <stas.versilov@gmail.com>
2013-01-13 23:53:44 +0400

    Display site division mark for child subpages (like project/subproject URLs).

--- app/helpers/site_helper.rb
+++ app/helpers/site_helper.rb
@@ -1,6 +1,14 @@
 module SiteHelper
+  def current_path_includes?(path)
+    if path.length > 1
+      request.path.start_with?(path) 
+    else
+      current_page?(path)
+    end
+  end
+
   def nav_link(link_text, link_path)
-    class_name = current_page?(link_path) ? 'active' : ''
+    class_name = current_path_includes?(link_path) ? 'active' : ''
 
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
